8cf3b3d0b4585bac51e0a374638ce3d3d7a207ea
Francisco Manzano Magaña <keoh77@gmail.com>
2018-03-03 01:15:22 +0100

    Settings prod

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -2,7 +2,7 @@ import os
 
 from .base import *
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['biblio.framanmag.com', 'webbiblio.herokuapp.com']
 
 DB_HOST = os.environ["DB_HOST"]
@@ -11,13 +11,7 @@ DB_PASS = os.environ["DB_PASS"]
 DB_USER = os.environ["DB_USER"]
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-S3_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-MEDIA_URL = S3_URL + 'media/'
+MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': {
